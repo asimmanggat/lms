e4be65df243205649c41450d7f5d53dedff6b19d
asimmanggat <asimmanggat@gmail.com>
2021-09-02 00:22:02 +0500

    Database connected and migrations done

--- lms/settings.py
+++ lms/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users',
+    'courses',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +77,11 @@ WSGI_APPLICATION = 'lms.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'lms',
+        'USER': 'asim',
+        'PASSWORD': 'Pakistan1234$',
+        'HOST': 'localhost'
     }
 }
 
