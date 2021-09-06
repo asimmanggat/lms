5dda642f7b329dbbaba35f8edb00331633614cce
Asim Nazir <asim>
2021-09-06 20:25:47 +0500

    Added Authentication

--- lms/urls.py
+++ lms/urls.py
@@ -20,5 +20,5 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("users.urls")),
+    path('', include("users.urls")),  
 ]
--- users/views.py
+++ users/views.py
@@ -1,9 +1,35 @@
-from django.http.request import HttpRequest
-from django.http.response import HttpResponse
+from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
 from django.shortcuts import render
-from django.http import HttpResponse, HttpRequest
+from django.http import HttpResponse, HttpRequest, HttpResponseRedirect
+from django.urls import reverse
+
 
 # Create your views here.
 
 def home(request):
-    return HttpResponse("Welcome Home!")
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect(reverse("login"))
+    return render(request, "home.html", {
+        "success_message": "Your successfully Logged In"
+    })
+    
+def login(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            auth_login(request, user)
+            return HttpResponseRedirect(reverse(home))
+        else:
+            return render(request, "login.html", {
+                "error_message": "Invalid Credentials!"
+            })
+    return render(request, "login.html")
+    
+
+def logout(request):
+    auth_logout(request)
+    return render(request, "login.html", {
+        "success_message": "Your successfully Logged Out"
+    })
